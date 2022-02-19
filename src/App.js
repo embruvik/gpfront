6d91fcd200c6e34287d547cfb0d7a99789451805
Erik Magnus Bruvik <ermabr@hotmail.com>
2022-02-19 12:19:14 +0100

    Add instr and .env to .gitignore

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import 'axios';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [message, setMessage]  = useState("No message yet!");
+
+  const sendToUser = (e) => {
+    e.preventDefault();
+    axios.get("http://localhost:8080/user/erik")
+    .then(resp => {
+      console.log(resp);
+      setMessage(resp.data.message);
+    })
+    .catch(error => {
+      setMessage(error.message);
+    });
+  }
+
+  const sendToInstr = (e) => {
+    e.preventDefault();
+    axios.get("http://localhost:8080/instr/sg562")
+    .then(resp => {
+      console.log(resp);
+      setMessage(resp.data);
+    })
+    .catch(error => {
+      setMessage(error.message);
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={sendToUser}> Send to user</button>
+      <button onClick={sendToInstr}> Send to instr</button>
+      { message ? <h1>{message}</h1> : <h1>Uhm, hello!</h1> }
     </div>
   );
 }
