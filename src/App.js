39c3cfdc14a133630176de8b642ea0badfbe9ff0
Erik Magnus Bruvik <ermabr@hotmail.com>
2022-02-24 10:41:51 +0100

    Add figure (from ml/py) to app

--- src/App.js
+++ src/App.js
@@ -7,13 +7,26 @@ import axios from 'axios';
 function App() {
 
   const [message, setMessage]  = useState("No message yet!");
+  const [gotfig, setGotfig] = useState(false);
+  const [figdata, setFigdata] = useState('');
 
   const sendToUser = (e) => {
     e.preventDefault();
-    axios.get("http://localhost:8080/user/erik")
+
+    let ml = '';
+    (Math.random() < 0.75) ? ml='ml/' : ml='';
+
+    axios.get(`http://localhost:8080/user/${ml}erik`)
     .then(resp => {
       console.log(resp);
       setMessage(resp.data.message);
+      if (resp.data.fig) {
+        setGotfig(true)
+        setFigdata(resp.data.fig)
+      } else {
+        setGotfig(false)
+      }
+
     })
     .catch(error => {
       setMessage(error.message);
@@ -37,6 +50,7 @@ function App() {
       <button onClick={sendToUser}> Send to user</button>
       <button onClick={sendToInstr}> Send to instr</button>
       { message ? <h1>{message}</h1> : <h1>Uhm, hello!</h1> }
+      { gotfig ? <img src={figdata}></img> : <h3>No figure in data...</h3>}
     </div>
   );
 }
